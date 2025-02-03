e2986ac54ad8aa941e21e71c990010cb7046f746
terria1020 <jaehan1346@gmail.com>
2025-02-03 17:49:45 +0900

    docs: 임시 home api 에 대한 api 문서화 추가

--- src/home/home.router.ts
+++ src/home/home.router.ts
@@ -3,6 +3,24 @@ import { Router, Request, Response } from "express";
 const homeRouter: Router = Router();
 
 // exam route
+// swagger-jsDoc를 사용한 swagger API 문서화
+/**
+ * @openapi
+ * /:
+ *   get:
+ *     description: Get home message
+ *     responses:
+ *       200:
+ *         description: Returns a test message
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 testMessage:
+ *                   type: string
+ *                   example: hello, ecommerce-service!
+ */
 homeRouter.get("/", (req, res) => {
     getHome(req, res);
 });
