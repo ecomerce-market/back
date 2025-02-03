e91839dfe67713fd01692ccad961123e0b4b5668
terria1020 <jaehan1346@gmail.com>
2025-02-03 21:02:01 +0900

    update: userSchema에 도큐먼트 autoCreate 옵션 추가

--- src/router/user/user.scheme.ts
+++ src/router/user/user.scheme.ts
@@ -1,14 +1,19 @@
 import { model, Model, Schema } from "mongoose";
 
-export const userSchema: Schema = new Schema({
-    _userId: Schema.Types.ObjectId,
-    loginId: String,
-    loginPw: String,
-    name: String,
-    email: String,
-    phone: String,
-    birth: String,
-    deleteAt: Schema.Types.Date,
-});
+export const userSchema: Schema = new Schema(
+    {
+        _userId: Schema.Types.ObjectId,
+        loginId: String,
+        loginPw: String,
+        name: String,
+        email: String,
+        phone: String,
+        birth: String,
+        deleteAt: Schema.Types.Date,
+    },
+    {
+        autoCreate: true,
+    }
+);
 
-export const userModel = model("User", userSchema) as Model<any>;
+export const userModel = model("user", userSchema) as Model<any>;
