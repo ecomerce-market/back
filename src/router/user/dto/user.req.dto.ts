7b5a53e17f514757706a3150d7b79fb8cb6bef25
terria1020 <jaehan1346@gmail.com>
2025-02-03 21:45:03 +0900

    create: user 회원가입 요청 dto 타입 추가

---
+++ src/router/user/dto/user.req.dto.ts
@@ -0,0 +1,10 @@
+type UserSignupReqDto = {
+    loginId: string;
+    loginPw: string;
+    name: string;
+    email: string;
+    phone: string;
+    address: string;
+    extraAddr: string;
+    birth: string;
+};
