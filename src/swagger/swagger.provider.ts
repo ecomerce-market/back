8ac83e88b3bd0c3630273d8530fa8b274f30d31e
terria1020 <jaehan1346@gmail.com>
2025-02-05 21:26:11 +0900

    update: swagger yml 정의 파일 분리 후 로드를 위한 설정 변경

--- src/swagger/swagger.provider.ts
+++ src/swagger/swagger.provider.ts
@@ -1,5 +1,24 @@
 import * as YAML from "yamljs";
 
-const swaggerDocs = YAML.load("swagger.yml");
+// const swaggerDocs = YAML.load("swagger/swagger.yml");
+
+const mergeYamlFiles = () => {
+    const baseSwagger = YAML.load("swagger/swagger.yml");
+
+    const userPath = YAML.load("swagger/paths/users.yml");
+    const authPath = YAML.load("swagger/paths/auth.yml");
+    const productPath = YAML.load("swagger/paths/products.yml");
+
+    return {
+        ...baseSwagger,
+        paths: {
+            ...authPath,
+            ...userPath,
+            ...productPath,
+        },
+    };
+};
+
+const swaggerDocs = mergeYamlFiles();
 
 export default swaggerDocs;
