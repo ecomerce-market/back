9281b511f36cf5556320898b0dfd921778d729a8
terria1020 <jaehan1346@gmail.com>
2025-02-05 20:58:59 +0900

    update: 상품 관련 path 추가

--- src/router/router.constants.ts
+++ src/router/router.constants.ts
@@ -1,3 +1,4 @@
 export const PATH_PREFIX = "/api/v1";
 export const PATH_USERS = PATH_PREFIX + "/users";
 export const PATH_AUTH = PATH_PREFIX + "/auth";
+export const PATH_PRODUCTS = PATH_PREFIX + "/products";
