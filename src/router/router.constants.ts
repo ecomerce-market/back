cd6d0477244912aa9a3d62dda1e363d64a6d6b19
terria1020 <jaehan1346@gmail.com>
2025-02-06 15:30:17 +0900

    update: 배너에 대한 path 추가

--- src/router/router.constants.ts
+++ src/router/router.constants.ts
@@ -2,3 +2,4 @@ export const PATH_PREFIX = "/api/v1";
 export const PATH_USERS = PATH_PREFIX + "/users";
 export const PATH_AUTH = PATH_PREFIX + "/auth";
 export const PATH_PRODUCTS = PATH_PREFIX + "/products";
+export const PATH_BANNERS = PATH_PREFIX + "/banners";
