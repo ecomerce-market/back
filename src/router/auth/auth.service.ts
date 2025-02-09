8e291927d50df6ace4e825e562581d41e8361a02
terria1020 <jaehan1346@gmail.com>
2025-02-09 11:38:21 +0900

    feat: 리프레시토큰을 이용한 accessToken 재발급 route 및 서비스 추가

--- src/router/auth/auth.service.ts
+++ src/router/auth/auth.service.ts
@@ -44,6 +44,50 @@ class AuthService {
             return;
         }
     }
+
+    async recreateAccessToken(req: Request, res: Response) {
+        if (validateMiddleware.validateCheck(req, res)) {
+            return;
+        }
+
+        const authorization = req.headers.authorization;
+        console.log("authorization: ", authorization);
+        const refreshToken = authorization?.split("Bearer ")[1];
+        if (!refreshToken) {
+            res.status(401).json({
+                message: "Unauthorized",
+                code: "E009",
+            });
+            return;
+        }
+        const result = jwtService.validateToken(refreshToken);
+
+        if (result instanceof Error) {
+            res.status(401).json({
+                message: "Unauthorized",
+                code: "E009",
+                error: result.message,
+            });
+            return;
+        } else if (!result) {
+            res.status(401).json({
+                message: "Unauthorized",
+                code: "E009",
+            });
+            return;
+        } else {
+            const decoded = jwtService.readToken(refreshToken);
+
+            const { accessToken } = jwtService.writeToken({
+                loginId: decoded.loginId,
+                email: decoded.email,
+            });
+            res.status(200).json({
+                accessToken,
+            });
+            return;
+        }
+    }
 }
 
 export default AuthService;
