42324615dc4ab3a113852b8d0c4a75efa6754c74
terria1020 <jaehan1346@gmail.com>
2025-02-10 15:53:18 +0900

    update: 마감 임박 상품 응답 dto 추가, 상품 req, res dto 모듈 일부 수정

--- src/router/product/dto/product.req.dto.ts
+++ src/router/product/dto/product.req.dto.ts
@@ -1,8 +1,17 @@
-namespace ProductReqDto {
-    export interface GetCategoryParam {
-        id?: number;
-        child?: boolean;
-        name?: string;
-        depth?: number;
+namespace ProductReqDto {}
+
+export interface GetCategoryParam {
+    id?: number;
+    child?: boolean;
+    name?: string;
+    depth?: number;
+}
+export class GetEndingSoonParam {
+    pageSize: number;
+    pageNumber: number;
+
+    constructor(pageSize: number, pageOffset: number) {
+        this.pageSize = pageSize ?? 10;
+        this.pageNumber = pageOffset ?? 1;
     }
 }
