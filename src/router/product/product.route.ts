d9ef5c41f14ef67905204e4f541e457e5532ef2f
terria1020 <jaehan1346@gmail.com>
2025-02-11 23:18:31 +0900

    feat: 상품 상세 조회 api 추가, 기존 상품 목록조회에서 productId 키의 값을 _id 로 변경

--- src/router/product/product.route.ts
+++ src/router/product/product.route.ts
@@ -11,6 +11,11 @@ productRouter.get(PATH_PRODUCTS, (req, res) => {
     productService.getProducts(req, res);
 });
 
+// 상품 상세 조회
+productRouter.get(PATH_PRODUCTS + "/:productId", (req, res) => {
+    productService.getProductDetail(req, res);
+});
+
 // 상품에 대한 카테고리 조회
 productRouter.get(PATH_PRODUCTS + "/categories", (req, res) => {
     productService.getCategories(req, res);
