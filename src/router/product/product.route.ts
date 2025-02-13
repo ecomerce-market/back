4de4228734f0a1b3eae501548f94bac7045d4d09
terria1020 <jaehan1346@gmail.com>
2025-02-13 18:25:24 +0900

    fix: path 등록 문제로 주말 상품 조회 api 등이 상품 상세조회로 읽히는 문제 수정

--- src/router/product/product.route.ts
+++ src/router/product/product.route.ts
@@ -12,6 +12,26 @@ productRouter.get(PATH_PRODUCTS, (req, res) => {
     productService.getProducts(req, res);
 });
 
+// 상품에 대한 카테고리 조회
+productRouter.get(PATH_PRODUCTS + "/categories", (req, res) => {
+    productService.getCategories(req, res);
+});
+
+// 주말 할인 특가 상품 목록 조회
+productRouter.get(PATH_PRODUCTS + "/weekend-deals", (req, res) => {
+    productService.getWeekendDeals(req, res);
+});
+
+// 마감 임박 상품 목록 조회
+productRouter.get(PATH_PRODUCTS + "/ending-soon", (req, res) => {
+    productService.getEndingSoon(req, res);
+});
+
+// 신상품 목록 조회
+productRouter.get(PATH_PRODUCTS + "/new-products", (req, res) => {
+    productService.getNewProducts(req, res);
+});
+
 // 상품 상세 조회
 productRouter.get(PATH_PRODUCTS + "/:productId", (req, res) => {
     productService.getProductDetail(req, res);
@@ -34,25 +54,5 @@ productRouter.delete(
         productService.unlikeProduct(req, res);
     }
 );
-
-// 상품에 대한 카테고리 조회
-productRouter.get(PATH_PRODUCTS + "/categories", (req, res) => {
-    productService.getCategories(req, res);
-});
-
-// 주말 할인 특가 상품 목록 조회
-productRouter.get(PATH_PRODUCTS + "/weekend-deals", (req, res) => {
-    productService.getWeekendDeals(req, res);
-});
-
-// 마감 임박 상품 목록 조회
-productRouter.get(PATH_PRODUCTS + "/ending-soon", (req, res) => {
-    productService.getEndingSoon(req, res);
-});
-
-// 신상품 목록 조회
-productRouter.get(PATH_PRODUCTS + "/new-products", (req, res) => {
-    productService.getNewProducts(req, res);
-});
 
 export default productRouter;
