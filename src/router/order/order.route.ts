9dbe4c59d403f234d6fee8c93733efb1b638c6ac
terria1020 <jaehan1346@gmail.com>
2025-02-16 20:46:25 +0900

    update: 주문서 수정에 대한 validate 추가

--- src/router/order/order.route.ts
+++ src/router/order/order.route.ts
@@ -8,16 +8,19 @@ const orderRouter = Router();
 
 const orderService = new OrderService();
 
+// 주문서 생성
 orderRouter.post(PATH_ORDERS + "/", jwtMiddleware.jwtMiddleWare, (req, res) => {
     orderService.createOrder(req, res);
 });
 
+// 주문서 수정 (쿠폰 사용, 포인트 사용, 결제수단 변경, 배송지 변경)
 orderRouter.patch(
     PATH_ORDERS + "/:orderId",
     jwtMiddleware.jwtMiddleWare,
     body("paymentMethod").optional().isString(),
     body("usePoint").optional().isNumeric(),
     body("coupon").optional().isString().isMongoId(),
+    body("userAddressInfo").optional().isString().isMongoId(),
     (req, res) => {
         orderService.updateOrder(req, res);
     }
