e42eaecc4d08876db377a7adb57a168aedaeaf51
terria1020 <jaehan1346@gmail.com>
2025-02-20 16:33:26 +0900

    refactor: 배너 조회 서비스 및 route에서 resDto 사용 등

--- src/router/banner/banner.service.ts
+++ src/router/banner/banner.service.ts
@@ -3,27 +3,17 @@ import { ParamsDictionary } from "express-serve-static-core";
 import { ParsedQs } from "qs";
 import mainBannerRepository from "./repository/mainBanner.repository";
 import { MainBannerResDto } from "./dto/mainBanner.res.dto";
+import { ResDto } from "../../common/dto/common.res.dto";
 
 class BannerService {
-    async getMainBanners(req: Request, res: Response) {
+    async getMainBanners(req: Request, res: Response): Promise<ResDto> {
         const banners = await mainBannerRepository.findAll();
 
-        const bannerDto: Array<MainBannerResDto> = [];
+        const bannerDto: Array<MainBannerResDto> = banners.map(
+            (bannner) => new MainBannerResDto(bannner)
+        );
 
-        banners.forEach((banner) => {
-            bannerDto.push({
-                name: banner.name,
-                imgUrl: banner.imgUrl,
-                link: banner.link,
-                endAt: banner.endAt,
-                displayOrder: banner.displayOrder,
-            });
-        });
-
-        return res.status(200).json({
-            message: "success",
-            banners: bannerDto,
-        });
+        return new ResDto({ data: { banners: bannerDto } });
     }
 }
 
