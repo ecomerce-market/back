fb8394bf27f0b411292fa9c2b600e17b34d61f59
terria1020 <jaehan1346@gmail.com>
2025-02-21 19:32:08 +0900

    create: 비밀번호 재설정을 위한 resetToken model 및 repository 추가

---
+++ src/router/accounts/repository/resetToken.repository.ts
@@ -0,0 +1,26 @@
+import { resetTokenModel } from "../model/resetToken.schema";
+
+class ResetTokenRepository {
+    async update(resetToken: any) {
+        return resetTokenModel.updateOne(
+            { _id: resetToken._id },
+            {
+                $set: resetToken,
+            }
+        );
+    }
+    async create(userId: string) {
+        return resetTokenModel.create({ userId: userId });
+    }
+
+    async findById(id: string) {
+        return resetTokenModel.findById(id);
+    }
+
+    async findByUserId(userId: string) {
+        return resetTokenModel.findOne({ userId: userId });
+    }
+}
+
+const resetTokenRepository: ResetTokenRepository = new ResetTokenRepository();
+export default resetTokenRepository;
